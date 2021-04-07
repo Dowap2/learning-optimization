4f3de48dac35636515c7e7d063af667f4b0fd80a
dowap2 <dowapdowari@gmail.com>
2021-04-07 21:07:46 +0900

    add lazy load

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://lh3.googleusercontent.com/proxy/G_r7q-TfrZ-h-4e9te1787QgkgrpRnTvZHlJHk-4MBPaFV0z3r5XmeIZ34CwiRlOlosG52DEynzfdzVehJ5V0jwKgSbQMOBqJks7v0ZxhUj1BtNT2lcioBLA4XYewqho3EMub6sajgplevBU12Kk3sDGjPOy7L4F1HxjnUnKO76dqFM5Fg8-x4nG-s-rThawD0ghlzm1jNblIsDJNwhPycGkJ7W30JXF5v34oo49yl-K7NvsyN04DGNAuXSwLwEE-BjWckVSUjsYgJ3SYJc5BfMJdGqY9bcWnT9vQt2S7g"
+          className="picture"
+          alt="logo"
+        />
+        <img
+          src="https://lh3.googleusercontent.com/proxy/G_r7q-TfrZ-h-4e9te1787QgkgrpRnTvZHlJHk-4MBPaFV0z3r5XmeIZ34CwiRlOlosG52DEynzfdzVehJ5V0jwKgSbQMOBqJks7v0ZxhUj1BtNT2lcioBLA4XYewqho3EMub6sajgplevBU12Kk3sDGjPOy7L4F1HxjnUnKO76dqFM5Fg8-x4nG-s-rThawD0ghlzm1jNblIsDJNwhPycGkJ7W30JXF5v34oo49yl-K7NvsyN04DGNAuXSwLwEE-BjWckVSUjsYgJ3SYJc5BfMJdGqY9bcWnT9vQt2S7g"
+          className="picture"
+          loading="lazy"
+          alt="logo"
+        />
+        <img
+          src="https://lh3.googleusercontent.com/proxy/G_r7q-TfrZ-h-4e9te1787QgkgrpRnTvZHlJHk-4MBPaFV0z3r5XmeIZ34CwiRlOlosG52DEynzfdzVehJ5V0jwKgSbQMOBqJks7v0ZxhUj1BtNT2lcioBLA4XYewqho3EMub6sajgplevBU12Kk3sDGjPOy7L4F1HxjnUnKO76dqFM5Fg8-x4nG-s-rThawD0ghlzm1jNblIsDJNwhPycGkJ7W30JXF5v34oo49yl-K7NvsyN04DGNAuXSwLwEE-BjWckVSUjsYgJ3SYJc5BfMJdGqY9bcWnT9vQt2S7g"
+          className="picture"
+          loading="lazy"
+          alt="logo"
+        />
+        <img
+          src="https://lh3.googleusercontent.com/proxy/G_r7q-TfrZ-h-4e9te1787QgkgrpRnTvZHlJHk-4MBPaFV0z3r5XmeIZ34CwiRlOlosG52DEynzfdzVehJ5V0jwKgSbQMOBqJks7v0ZxhUj1BtNT2lcioBLA4XYewqho3EMub6sajgplevBU12Kk3sDGjPOy7L4F1HxjnUnKO76dqFM5Fg8-x4nG-s-rThawD0ghlzm1jNblIsDJNwhPycGkJ7W30JXF5v34oo49yl-K7NvsyN04DGNAuXSwLwEE-BjWckVSUjsYgJ3SYJc5BfMJdGqY9bcWnT9vQt2S7g"
+          className="picture"
+          loading="lazy"
+          alt="logo"
+        />
+        <img
+          src="https://lh3.googleusercontent.com/proxy/G_r7q-TfrZ-h-4e9te1787QgkgrpRnTvZHlJHk-4MBPaFV0z3r5XmeIZ34CwiRlOlosG52DEynzfdzVehJ5V0jwKgSbQMOBqJks7v0ZxhUj1BtNT2lcioBLA4XYewqho3EMub6sajgplevBU12Kk3sDGjPOy7L4F1HxjnUnKO76dqFM5Fg8-x4nG-s-rThawD0ghlzm1jNblIsDJNwhPycGkJ7W30JXF5v34oo49yl-K7NvsyN04DGNAuXSwLwEE-BjWckVSUjsYgJ3SYJc5BfMJdGqY9bcWnT9vQt2S7g"
+          className="picture"
+          loading="lazy"
+          alt="logo"
+        />
+        <img
+          src="https://lh3.googleusercontent.com/proxy/G_r7q-TfrZ-h-4e9te1787QgkgrpRnTvZHlJHk-4MBPaFV0z3r5XmeIZ34CwiRlOlosG52DEynzfdzVehJ5V0jwKgSbQMOBqJks7v0ZxhUj1BtNT2lcioBLA4XYewqho3EMub6sajgplevBU12Kk3sDGjPOy7L4F1HxjnUnKO76dqFM5Fg8-x4nG-s-rThawD0ghlzm1jNblIsDJNwhPycGkJ7W30JXF5v34oo49yl-K7NvsyN04DGNAuXSwLwEE-BjWckVSUjsYgJ3SYJc5BfMJdGqY9bcWnT9vQt2S7g"
+          className="picture"
+          loading="lazy"
+          alt="logo"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
