143a812d758c77631a600888a7992bcd67a62604
dowap2 <dowapdowari@gmail.com>
2021-04-08 18:14:22 +0900

    upload img

--- src/UploadImage.jsx
+++ src/UploadImage.jsx
@@ -1,31 +1,33 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const ImageBox = styled.img`
+  width: 500px;
+  height: 500px;
+`;
 
 export function UploadImage(props) {
   const [url, setUrl] = useState("");
-  const [imgFile, setImgFile] = useState(null);
 
   const changeFile = event => {
     let reader = new FileReader();
 
-    console.log(event);
     reader.onloadend = e => {
       const base64 = reader.result;
       if (base64) {
         setUrl(base64.toString());
       }
     };
-    if (event.target.files[0]) {
-      reader.readAsDataURL(event.target.files[0]);
-      setImgFile(event.target.files[0]);
-      console.log(imgFile);
+    if (event.files[0]) {
+      reader.readAsDataURL(event.files[0]);
     }
   };
   return (
     <div>
       <div>
-        <img src={url} alt="img" />
+        <ImageBox src={url} alt="img" />
       </div>
-      <input type="file" accept="img/*" onChange={e => changeFile(e)} />
+      <input type="file" accept="img/*" onChange={e => changeFile(e.target)} />
     </div>
   );
 }
