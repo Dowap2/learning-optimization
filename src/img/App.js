7cd09f07ddeb7cb9de8fc5d51e9397f2005b0b43
dowap2 <dowapdowari@gmail.com>
2021-04-21 16:40:03 +0900

    add useMemo

---
+++ src/img/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useMemo } from "react";
+import propTypes from "prop-types";
+
+function Q3() {
+  const [count, setCount] = useState(0);
+
+  const plusOne = useMemo(() => setCount(count + 1));
+
+  return (
+    <div>
+      <Child name="COUNTER1" />
+      <>: </>
+      {count}
+      <> </>
+      <button onClick={plusOne}>+</button>
+    </div>
+  );
+}
+export default Q3;
+
+const Child = ({ name }) => {
+  return <span style={{ color: "red" }}>{name}</span>;
+};
+
+Child.propTypes = {
+  name: propTypes.string.isRequired
+};
