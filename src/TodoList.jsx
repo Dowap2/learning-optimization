064dc3b9da03fb685edb30dec6314c4e896cf964
dowap2 <dowapdowari@gmail.com>
2021-04-27 20:37:11 +0900

    add blog example code

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,24 +1,35 @@
+import { set } from "mongoose";
 import React, { useCallback, useState, useMemo } from "react";
 
 export function TodoList() {
   const [todo, setTodo] = useState("");
   const [list, setList] = useState([]);
+  const [htmlList, setHtmlList] = useState([]);
   const [listLength, setListLength] = useState(0);
 
-  const setListFunc = () => {
-    setList(list.concat(todo));
+  const setListFunc = useCallback(todo => {
+    setList(list => list.concat(todo));
     setTodo("");
-  };
+  }, []);
+
+  const addHtmlToList = useMemo(() => {
+    for (const item in list) {
+      console.log(list[item]);
+      setHtmlList(htmlList.concat(<div>{list[item]}</div>));
+    }
+  }, [list]);
 
   const checkListLength = useMemo(() => {
     setListLength(list.length);
   }, [list]);
+
   return (
     <div>
       <input type="text" value={todo} onChange={e => setTodo(e.target.value)} />
-      <button onClick={e => setListFunc()}></button>
-      <div>{list}</div>
+      <button onClick={e => setListFunc(todo)}></button>
+      <div>{htmlList}</div>
       <div>{"todo수 :" + listLength}</div>
+      <button onClick={e => console.log(list)}></button>
     </div>
   );
 }
