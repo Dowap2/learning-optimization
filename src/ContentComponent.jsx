b66b8dcffda891f5ef566543c60feab9aec430c6
dowap2 <dowapdowari@gmail.com>
2021-05-06 19:24:09 +0900

    add slide animation

--- src/ContentComponent.jsx
+++ src/ContentComponent.jsx
@@ -1,21 +1,73 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 
 const ContentBox = styled.div`
-  width: 100px;
-  height: 500px;
+  width: 400px;
+  height: 400px;
 `;
-const ImgBox = styled.img`
-  width: 100px;
-  height: 100px;
+const ImgBox = styled.div`
+  height: 400px;
+  overflow-x: hidden;
+`;
+const ImgList = styled.div`
+  width: 2000px;
+  height: 400px;
+  transform: ${props => props.transform};
+  transition: 2s;
+`;
+const Img = styled.img`
+  width: 400px;
+  height: 400px;
+  display: table;
+  float: left;
 `;
 
 export function ContentComponent(props) {
+  const index = useSelector(state => state.slideState.slideState);
+  const transform = `translate3d(-${(index % 6) * 400}px, 0, 0);`;
   return (
-    <ContentBox>
-      {props.value}
-      <ImgBox src={props.img} alt="img" />
-      <button onClick={e => console.log(props.value)}></button>
-    </ContentBox>
+    <div>
+      <ContentBox>
+        <ImgBox>
+          <ImgList transform={transform}>
+            <Img
+              src={"https://pbs.twimg.com/media/ExUElF7VcAMx7jx.jpg"}
+              alt="img"
+            />
+            <Img
+              src={
+                "https://image.bugsm.co.kr/artist/images/1000/800491/80049126.jpg"
+              }
+              alt="img"
+            />
+            <Img
+              src={
+                "https://pds.joins.com/news/component/htmlphoto_mmdata/202103/30/0d115e5d-a4fe-4ff8-9689-6b77a730d19d.jpg"
+              }
+              alt="img"
+            />
+            <Img
+              src={
+                "https://mblogthumb-phinf.pstatic.net/MjAxOTExMjlfMjA3/MDAxNTc0OTc2Nzg1MzMy.WJhzcrbtitmAgLJDtqwebCR1hDQbxcQUPvAvdxpk5O8g.vCs9wsVGbokP2KX1LUkr4hXoeEkRZYTzGkSGmFNeZ7og.JPEG.pomon64/iumini5mvkbk_201.jpg?type=w800"
+              }
+              alt="img"
+            />
+            <Img
+              src={
+                "http://www.newsfreezone.co.kr/news/photo/202011/277190_272039_1050.jpg"
+              }
+              alt="img"
+            />
+            <Img
+              src={
+                "https://blog.kakaocdn.net/dn/bjoIyf/btq0NvekYvN/4alKdCqujcJjSf10JjDAy0/img.jpg"
+              }
+              alt="img"
+            />
+          </ImgList>
+        </ImgBox>
+      </ContentBox>
+    </div>
   );
 }
--- src/SliderContent.jsx
+++ src/SliderContent.jsx
@@ -1,52 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
 import { ContentComponent } from "./ContentComponent";
 import { ContentButtonContainer } from "./ContentButtonContainer";
 
 export function SliderContent() {
-  const index = useSelector(state => state.slideState.slideState);
-  const [url, setUrl] = useState("");
-  const changeImg = useEffect(() => {
-    switch (index % 6) {
-      case 0:
-        setUrl("https://pbs.twimg.com/media/ExUElF7VcAMx7jx.jpg");
-        break;
-      case 1:
-        setUrl(
-          "https://image.bugsm.co.kr/artist/images/1000/800491/80049126.jpg"
-        );
-        break;
-      case 2:
-        setUrl(
-          "https://pds.joins.com/news/component/htmlphoto_mmdata/202103/30/0d115e5d-a4fe-4ff8-9689-6b77a730d19d.jpg"
-        );
-        break;
-      case 3:
-        setUrl(
-          "https://mblogthumb-phinf.pstatic.net/MjAxOTExMjlfMjA3/MDAxNTc0OTc2Nzg1MzMy.WJhzcrbtitmAgLJDtqwebCR1hDQbxcQUPvAvdxpk5O8g.vCs9wsVGbokP2KX1LUkr4hXoeEkRZYTzGkSGmFNeZ7og.JPEG.pomon64/iumini5mvkbk_201.jpg?type=w800"
-        );
-        break;
-      case 4: {
-        setUrl(
-          "http://www.newsfreezone.co.kr/news/photo/202011/277190_272039_1050.jpg"
-        );
-        break;
-      }
-      case 5:
-        setUrl(
-          "https://blog.kakaocdn.net/dn/bjoIyf/btq0NvekYvN/4alKdCqujcJjSf10JjDAy0/img.jpg"
-        );
-        break;
-      default:
-        setUrl(
-          "https://img2.sbs.co.kr/img/sbs_cms/WE/2019/08/09/WE97496996_ori.jpg"
-        );
-        break;
-    }
-  }, [index]);
   return (
     <div>
-      <ContentComponent value={index} img={url} />
+      <ContentComponent />
       <ContentButtonContainer />
     </div>
   );
