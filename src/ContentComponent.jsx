4d801265b744d7ddf35d0f10a557f14115f79d5f
dowap2 <dowapdowari@gmail.com>
2021-05-06 19:28:10 +0900

    modify slide length

--- src/ContentComponent.jsx
+++ src/ContentComponent.jsx
@@ -11,10 +11,10 @@ const ImgBox = styled.div`
   overflow-x: hidden;
 `;
 const ImgList = styled.div`
-  width: 2000px;
+  width: 2400px;
   height: 400px;
   transform: ${props => props.transform};
-  transition: 2s;
+  transition: 1s;
 `;
 const Img = styled.img`
   width: 400px;
@@ -33,37 +33,37 @@ export function ContentComponent(props) {
           <ImgList transform={transform}>
             <Img
               src={"https://pbs.twimg.com/media/ExUElF7VcAMx7jx.jpg"}
-              alt="img"
+              alt="1"
             />
             <Img
               src={
                 "https://image.bugsm.co.kr/artist/images/1000/800491/80049126.jpg"
               }
-              alt="img"
+              alt="2"
             />
             <Img
               src={
                 "https://pds.joins.com/news/component/htmlphoto_mmdata/202103/30/0d115e5d-a4fe-4ff8-9689-6b77a730d19d.jpg"
               }
-              alt="img"
+              alt="3"
             />
             <Img
               src={
                 "https://mblogthumb-phinf.pstatic.net/MjAxOTExMjlfMjA3/MDAxNTc0OTc2Nzg1MzMy.WJhzcrbtitmAgLJDtqwebCR1hDQbxcQUPvAvdxpk5O8g.vCs9wsVGbokP2KX1LUkr4hXoeEkRZYTzGkSGmFNeZ7og.JPEG.pomon64/iumini5mvkbk_201.jpg?type=w800"
               }
-              alt="img"
+              alt="4"
             />
             <Img
               src={
                 "http://www.newsfreezone.co.kr/news/photo/202011/277190_272039_1050.jpg"
               }
-              alt="img"
+              alt="5"
             />
             <Img
               src={
-                "https://blog.kakaocdn.net/dn/bjoIyf/btq0NvekYvN/4alKdCqujcJjSf10JjDAy0/img.jpg"
+                "http://www.newscj.com/news/photo/202009/newscj_%EC%B2%9C%EC%A7%80%EC%9D%BC%EB%B3%B4_780873_817199_330.jpg"
               }
-              alt="img"
+              alt="6"
             />
           </ImgList>
         </ImgBox>
