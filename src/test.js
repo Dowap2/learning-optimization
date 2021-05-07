bfd96725d8696e4f6d60231d5f61a7ec18c4ead3
dowap2 <dowapdowari@gmail.com>
2021-05-07 21:58:04 +0900

    use match function

--- src/test.js
+++ src/test.js
@@ -24,3 +24,37 @@ test("return a user object", () => {
     email: `user1@test.com`
   });
 });
+
+//true와 false를 테스트
+test("number 0 is falsy 1 is truthy", () => {
+  expect(0).toBeFalsy();
+  expect(1).toBeTruthy();
+});
+
+//위에순으로 함수의 길이 특정원소 체크
+test("array", () => {
+  const colors = ["Red", "Yellow", "Blue"];
+  expect(colors).toHaveLength(3);
+  expect(colors).toContain("Yellow");
+  expect(colors).not.toContain("Green");
+});
+
+//정규식 체크
+test("string", () => {
+  expect(getUser(1).email).toBe("user1@test.com");
+  expect(getUser(2).email).toMatch(/.*test.com$/);
+});
+
+function getUserError(id) {
+  if (id <= 0) throw new Error("Invalid ID");
+  return {
+    id,
+    email: `user${id}@test.com`
+  };
+}
+
+//예외처리
+test("throw when id is non negative", () => {
+  expect(getUserError(-1)).toThrow();
+  expect(getUserError(-1)).toThrow("Invalid ID");
+});
