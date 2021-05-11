189aa6bf717f5b5c837dc2d812e2f81f8178e430
dowap2 <dowapdowari@gmail.com>
2021-05-11 19:37:27 +0900

    add click test file

--- src/SlideHeader.jsx
+++ src/SlideHeader.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
 
 export function SlideHeader(props) {
   return (
--- src/SlideHeader.test.jsx
+++ src/SlideHeader.test.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { SlideHeader } from "./SlideHeader";
 import { render } from "@testing-library/react";
+import { mount } from "enzyme";
+import { useSelector } from "react-redux";
 
 let component = null;
 
@@ -8,4 +10,11 @@ test("test render", () => {
   component = render(<SlideHeader />);
   const test = component;
   console.log(test);
+});
+
+test("button click", () => {
+  const wrapper = mount(<SlideHeader />);
+  const index = useSelector(state => state.slideState.slideState);
+  wrapper.find("button").simulate("click");
+  expect(index).toEqual(600);
 });
