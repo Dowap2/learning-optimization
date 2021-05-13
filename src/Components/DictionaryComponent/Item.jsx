cf3e8ae0a265ff166168eab6fceeccd16f6be385
dowap2 <dowapdowari@gmail.com>
2021-05-13 18:30:18 +0900

    fixed error

--- src/Components/DictionaryComponent/Item.jsx
+++ src/Components/DictionaryComponent/Item.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import styled from "styled-components";
+
+const ItemBox = styled.div`
+  width: 800px;
+  height: 1000px;
+  border: 1px solid #000;
+`;
 
 export function Item() {
-  return <div>Item</div>;
+  return <ItemBox>Item</ItemBox>;
 }
--- src/Components/DictionaryComponent/Search.jsx
+++ src/Components/DictionaryComponent/Search.jsx
@@ -1,39 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
 import styled from "styled-components";
 
-const SearchBar = styled.div`
+const SearchBar = styled.input`
   width: 800px;
   height: 40px;
   border: 1px solid #000;
 `;
-const Tag = styled.div`
-  width: 80px;
-  height: 20px;
-  float: left;
-  text-align: center;
-`;
 
 export function Search(props) {
-  const value = useSelector(state => state.filterState);
-  const [isTrueArray, setIsTrueArray] = useState([]);
-  const [htmlTag, setHtmlTag] = useState([]);
-  useEffect(() => {
-    let isTrue = [];
-    for (const key in value) {
-      if (value[key] === true) {
-        isTrue = isTrue.concat([key]);
-      }
-    }
-    setIsTrueArray(isTrue);
-  }, [value]);
-
-  useEffect(() => {
-    setHtmlTag([]);
-    for (let i = 0; i < isTrueArray.length; i++) {
-      setHtmlTag(htmlTag.concat([<Tag>{isTrueArray[i]}</Tag>]));
-    }
-  }, [isTrueArray]);
-
-  return <SearchBar>{htmlTag}</SearchBar>;
+  return <SearchBar placeholder="아이템 검색"></SearchBar>;
 }
